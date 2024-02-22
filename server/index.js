7b40d7b4eaf9941d615320f47c15554104df6235
Simon Leong <simonremgmt@gmail.com>
2024-02-21 23:50:26 -0500

    [refactor] updated cors and origins with respective server and frontend links

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const RegisterModel = require('./models/Register')
 const app = express()
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://mern-app-frontend-navy.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
