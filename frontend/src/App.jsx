52eddf1ffa6a352d01cce96a1b81deed1d5ab5c0
Simon Leong <simonremgmt@gmail.com>
2024-02-22 19:49:49 -0500

    cors v20

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     // .catch(err => console.log(err))
 
     try {
-      const response = await fetch("https://mern-app-api-seven.vercel.app/register", {
+      const response = await fetch("https://mern-app-api-seven.vercel.app/api/register", {
             method: "POST",
             headers: {
               "Content-Type": "application/json"
